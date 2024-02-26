8788ee2e49ca6f6df6466590d240debc19835696
horhe579 <georgi.i.traykov.2018@elsys-bg.org>
2024-02-26 15:59:26 +0200

    kur commit

---
+++ Pedal/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Pedal.Models;
+using System.Text.RegularExpressions;
+
+namespace Pedal.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _botUser;
+
+        public ChatHub()
+        {
+            _botUser = "MyChat Bot";
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser,
+                $"{userConnection.Name} has joined {userConnection.Room}");
+
+        }
+    }
+}
---
+++ Pedal/Models/UserConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace Pedal.Models
+{
+    public class UserConnection
+    {
+        public string Name { get; set; }
+        public string Room { get; set; }
+    }
+}
