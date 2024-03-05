1a205c4f703813c98dc77ebb360d2223995550da
horhe579 <georgi.i.traykov.2018@elsys-bg.org>
2024-03-05 14:46:22 +0200

    poopravih neshtata

--- Pedal/Hubs/ChatHub.cs
+++ Pedal/Hubs/ChatHub.cs
@@ -1,36 +1,15 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using Pedal.Models;
-<<<<<<< Updated upstream
 using System.Text.RegularExpressions;
-=======
->>>>>>> Stashed changes
 
 namespace Pedal.Hubs
 {
     public class ChatHub : Hub
     {
-        private readonly string _botUser;
-
-        public ChatHub()
-        {
-            _botUser = "MyChat Bot";
-        }
-<<<<<<< Updated upstream
-        public async Task JoinRoom(UserConnection userConnection)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
-            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser,
-                $"{userConnection.Name} has joined {userConnection.Room}");
-
-=======
         public async Task NewMessage(string SenderID, string ReceiverID, string Text, DateTime Timestamp)
         {
-            //await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
-            //await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser,
-            //$"{userConnection.User} has joined {userConnection.Room}");
             await Clients.All.SendAsync("ReceiveNewMessage",
                 SenderID, ReceiverID, Text, Timestamp);
->>>>>>> Stashed changes
         }
     }
 }
--- Pedal/Models/UserConnection.cs
+++ Pedal/Models/UserConnection.cs
@@ -2,11 +2,8 @@
 {
     public class UserConnection
     {
-<<<<<<< Updated upstream
         public string Name { get; set; }
-=======
         public string User { get; set; }
->>>>>>> Stashed changes
         public string Room { get; set; }
     }
 }
